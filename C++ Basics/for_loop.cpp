0d658f4afaf8fc3626355d76862eac02cc51e526
Krishna Shelar <168703616+Krish1712204@users.noreply.github.com>
2025-04-16 21:38:34 +0530

    Basics of C++
    Basics of C++

---
+++ C++ Basics/for_loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << "HELLO KRISHNA " << i << "'th iteration" << endl;
+    }
+    return 0;
+}
